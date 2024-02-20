e12616188b36fc7dbf3177191c590ff605b647b9
Elvis <e-pin@outlook.it>
2024-02-21 00:15:33 +0100

    asdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { fa, faker, th, tr } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomSentence = faker.lorem.lines(1);
+  userInput = '';
+  isCorrect = false;
+
+
+  handleInput(event: Event) {
+    var value = (event.target as HTMLInputElement).value;
+    this.userInput = value;
+
+    if (this.userInput === this.randomSentence) {
+      this.isCorrect = true;
+    }
+  }
+
+  getClass(i: number) {
+    if (!this.userInput[i]) return 'has-text-grey-dark';
+
+    return this.userInput[i] === this.randomSentence[i]
+      ? 'has-text-primary'
+      : 'has-text-danger';
+  }
 }
